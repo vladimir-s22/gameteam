2bc0df4d6bf12bb27ecb8624e7f8423281a993f8
Vladimir Shubodyorov <vladimir.shubodyorov@algosec.com>
2022-07-25 15:36:33 +0300

    reworked board scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public Image generalImage = null;
+    public Image generalHealth = null;
+
+    public int health = 5;
+    public int essence = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
