5bd6142bfbf4a0988942549268e5afe10e2d90a0
gamedevnifemi <oluwanifemiakinloose@gmail.com>
2022-08-07 23:36:35 +0100

    fixed screen size and sound, added mouse pointer

---
+++ Assets/Scripts/MuteScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteScript : MonoBehaviour
+{
+    [SerializeField] Image SoundOnIcon;
+    [SerializeField] Image SoundOffIcon;
+    private bool muted = false;
+    
+    void Start() 
+    {
+        if (!PlayerPrefs.HasKey("muted")) 
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else 
+        {
+            Load();
+        }
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
+    }
+
+    public void OnButtonPress() 
+    {
+        if(muted == false) 
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else 
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        Save();
+        UpdateButtonIcon();
+    }
+    private void UpdateButtonIcon() 
+    {
+        if (muted == false)
+        {
+            SoundOnIcon.enabled = true;
+            SoundOffIcon.enabled = false;
+        }
+        else 
+        {
+            SoundOnIcon.enabled = false;
+            SoundOffIcon.enabled = true;
+        }
+
+    }
+
+    private void Load() 
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save() 
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
